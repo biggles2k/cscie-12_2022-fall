93be4d54f63715a26af3128b58b8e1534f684655
Michael D. Hilborn <mhilborn@biggles2k.com>
2022-10-25 06:46:34 -0400

    Getting started with JavaScript... completed!

--- js_getting_started/scripts/css-mode.js
+++ js_getting_started/scripts/css-mode.js
@@ -1,1 +1,9 @@
 /* add your JavaScript code here! */
+document.addEventListener("DOMContentLoaded", function(e) {
+    var toggleBtn = document.getElementById('btn-mode');
+    toggleBtn.addEventListener("click", function(e) {
+        document.body.classList.toggle("darkmode");
+
+    });
+
+});
